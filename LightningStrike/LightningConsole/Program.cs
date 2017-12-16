9a13a302a59e134139fc7357f55d39e309b4ae9d
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-12-16 07:11:50 -0500

    Initial project stub commit

---
+++ LightningStrike/LightningConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LightningConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LightningStrike/LightningLibrary/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LightningLibrary
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
