1763a05ba9c98d07770ac92d19bf9892f28f4166
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-12-17 08:32:17 -0500

    changes

---
+++ LightningStrike/LightningLibrary/REST/RestLightning.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LightningLibrary.REST
+{
+
+    //http://api.lightning.community/?javascript#lnd-api-reference
+    public class RestLightning
+    {
+        public RestLightning()
+        {
+        }
+    }
+}
