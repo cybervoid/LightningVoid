a026c9908d2971e64a0c2329a9ef7fab91c9b9ad
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-12-30 10:36:01 -0500

    multi sig testing started

--- LightningStrike/LightningLibrary/Tests/LightningTests.cs
+++ LightningStrike/LightningLibrary/Tests/LightningTests.cs
@@ -9,6 +9,7 @@ using LightningLibrary.RPC;
 using LightningLibrary.Wallets;
 using LightningLibrary.Utilities;
 using NBitcoin;
+using LightningLibrary.Objects;
 
 namespace LightningLibrary.Tests
 {
@@ -45,9 +46,29 @@ namespace LightningLibrary.Tests
                 var key = GetKey(path, seed[i]);
                 var address = segwit.GetP2SHAddress(key);
                 keys.Add(key);
-                Console.WriteLine(address.ToString());
+                //Console.WriteLine(address.ToString());
             }
 
+            MultiSig multi = new MultiSig(NBitcoin.Network.TestNet);
+            var p2sh = multi.GetP2SHAddress(2, keys.ToArray());
+            Console.WriteLine(p2sh.ToString());
+            //multi: b2366808fa2396a5a32120a27f55571055491d6ff8e6bd1e31e52bdd14b91dfb
+
+            REST.BlockExplorer explorer = new REST.BlockExplorer("https://testnet.blockexplorer.com/");
+
+            //var tx = explorer.GetTransaction("b2366808fa2396a5a32120a27f55571055491d6ff8e6bd1e31e52bdd14b91dfb");
+
+            var response = explorer.GetUnspent(p2sh.ToString());
+            List<ExplorerUnspent> unspent = response.Convert<List<ExplorerUnspent>>();
+            List<Transaction> transactions = new List<Transaction>();
+            foreach (var item in unspent)
+            {
+                
+                ExplorerResponse txResponse = explorer.GetTransaction(item.txid);
+                RawFormat format = RawFormat.Satoshi;
+                var tx = Transaction.Parse(txResponse.data, format, Network.TestNet);
+                transactions.Add(tx);
+            }
             Console.ReadLine();
         }
 
@@ -55,6 +76,11 @@ namespace LightningLibrary.Tests
         {
             var wallet = new HDWallet(seed);
             return wallet.GetPrivateKey(path);
+        }
+
+        public void GetUnspent(ExtKey key)
+        {
+            //b5a4fa34d6ff45d66fcce713e8db9ff0308f1687dc83171bccdb1d17df84c7eb
         }
 
     }
--- LightningStrike/LightningLibrary/Utilities/MultiSig.cs
+++ LightningStrike/LightningLibrary/Utilities/MultiSig.cs
@@ -1,10 +1,105 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NBitcoin;
+
 namespace LightningLibrary.Utilities
 {
     public class MultiSig
     {
-        public MultiSig()
+        private Network _Network;
+
+        public MultiSig(Network network)
         {
+            this._Network = network;
         }
+
+        #region The combined public key and pub key script
+        /// <summary>
+        /// The equivalent of a public key
+        /// </summary>
+        /// <returns>The public key script.</returns>
+        /// <param name="minimumSignatures">Minimum signatures.</param>
+        /// <param name="keys">Keys.</param>
+        public Script GetPublicKeyScript(int minimumSignatures, PubKey[] keys)
+        {
+            //xxxx here
+            var scriptPubKey = PayToMultiSigTemplate.Instance.GenerateScriptPubKey(minimumSignatures, keys);
+            return scriptPubKey;
+        }
+
+        public Script GetPublicKeyScript(int minimumSignatures, Key[] keys)
+        {
+            List<PubKey> pubKeys = new List<PubKey>();
+            for (int i = 0; i < keys.Length; i++)
+            {
+                pubKeys.Add(keys[i].PubKey);
+            }
+            return GetPublicKeyScript(minimumSignatures, pubKeys.ToArray());
+        }
+
+        public BitcoinScriptAddress GetP2SHAddress(int minimumSignatures, ExtKey[] keys)
+        {
+            List<Key> k = new List<Key>();
+            for (int i = 0; i < keys.Length; i++)
+            {
+                k.Add(keys[i].PrivateKey);
+            }
+            return GetP2SHAddress(minimumSignatures, k.ToArray());
+        }
+        public BitcoinScriptAddress GetP2SHAddress(int minimumSignatures, Key[] keys)
+        {
+            Script pubKeyScript = GetPublicKeyScript(minimumSignatures, keys);
+            var address = pubKeyScript.WitHash.GetAddress(_Network);
+            var p2sh = address.GetScriptAddress();
+            return p2sh;
+        }
+
+
+        #endregion
+
+        #region Segwit Address
+        public BitcoinScriptAddress GetP2SHAddress(Key key)
+        {
+            //OLD
+            //This gives you a Bech32 address (currently not really interoperable in wallets, so you need to convert it into P2SH)
+            var address = key.PubKey.WitHash.GetAddress(_Network);
+            var p2sh = address.GetScriptAddress(); // This is P2SH address.
+            return p2sh;
+        }
+
+
+        /// <summary>
+        /// Gets the Segwit backward compatible address. (Send money to this address)
+        /// </summary>
+        /// <returns>The p2sh address.</returns>
+        public BitcoinScriptAddress GetP2SHAddress(ExtKey key)
+        {
+            return GetP2SHAddress(key.PrivateKey);
+        }
+
+        #endregion
+
+        #region Segwit Redeem Script
+        public Script GetRedeemScript(ExtKey key)
+        {
+            return GetRedeemScript(key.PrivateKey);
+        }
+
+        public Script GetRedeemScript(Key key)
+        {
+
+            return key.PubKey.WitHash.ScriptPubKey;
+        }
+
+
+        public Script CreateMultiSigScript(PubKey[] keys, int requiredSignatures = 1)
+        {
+            if (keys.Length == 0)
+                throw new ArgumentException("Required signatures cannot be 0.");
+            Script redeemScript = PayToMultiSigTemplate.Instance.GenerateScriptPubKey(requiredSignatures, keys);
+            return redeemScript;
+        }
+        #endregion
     }
 }
